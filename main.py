e946eabf9c28bb1ec806ae7c29e7363a002a11a6
teey <teey@com>
2026-04-18 20:07:00 +0800

    study

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+# 数据有类型,但是变量没有类型
+
+print(type(print_hi))
+# print(int("123.0"))
+print(float("123.333"))
+print(str(1234))
+# 标识符 变量 类 方法名字 大小写敏感 数字 英文 下划线 变量：全部小写 单词之间用_ 不是驼峰
+
+
